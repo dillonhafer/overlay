2510ebcb58e1b97e6d22c50af1f7c10095896074
Dillon Hafer <dh@dillonhafer.com>
2015-07-07 13:26:38 -0500

    Use object literal with overlay

--- overlay.js
+++ overlay.js
@@ -1,12 +1,25 @@
 (function() {
   "use strict"
+  function Overlay(overlay) {
+    this.html = overlay.html
+    this.urlRegex = new RegExp(overlay.pattern)
+
+    Overlay.prototype.createOverlay = function() {
+      var overlayDiv = document.createElement('div')
+      overlayDiv.innerHTML = this.html
+      document.body.appendChild(overlayDiv)
+    }
+
+    Overlay.prototype.shouldOverlay = function() {
+      return this.urlRegex.test(window.location.toString())
+    }
+  }
+
   chrome.storage.sync.get("overlays", function(data) {
     data.overlays.forEach(function(o) {
-      var re = new RegExp(o.pattern)
-      if(re.test(window.location.toString())) {
-        var overlayDiv = document.createElement('div')
-        overlayDiv.innerHTML = o.html
-        document.body.appendChild(overlayDiv)
+      var overlay = new Overlay(o)
+      if (overlay.shouldOverlay()) {
+        overlay.createOverlay()
       }
     })
   })
